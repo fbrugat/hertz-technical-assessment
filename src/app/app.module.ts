748944c6f35cb7af87e87845a633ca7b2f5dca8d
Ferran Brugat <ferran.brugat@gmail.com>
2019-05-11 19:38:45 +0100

    Updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { TransportsComponent } from './transports/transports.component';
+import { TransportComponent } from './transports/transport/transport.component';
+import { TransportListComponent } from './transports/transport-list/transport-list.component';
+import { TransportService } from './shared/transport.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransportsComponent,
+    TransportComponent,
+    TransportListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TransportService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
